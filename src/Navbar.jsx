0880f01fddaff9ccd6b2f93562a804887de052fb
tasneem123-UI <tasneemakhalid@gmail.com>
2025-08-19 19:47:51 +0300

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
           </button>
         </div>
         <div 
-          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`} 
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'} md:block`} 
           id="navbar-sticky"
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
